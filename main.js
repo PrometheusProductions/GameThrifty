9b3c3f855cf50fb2eb111e1445d7f5d01953c3aa
Marco Arellano <marcoarellano@Marcos-MacBook-Pro-3.local>
2020-11-13 11:19:13 -0500

    Added: search result returns, links to releventSearch, added alphabetical ordering of returnedResults, links are non-functional

--- main.js
+++ main.js
@@ -8,6 +8,19 @@
 // })
 //   .then(data => console.log(data))
 //   .catch(error => console.log('oops! data cannot be found.'))
+const showGame= (game) => {
+  const gameDiv = document.createElement('div')
+  gameDiv.classList.add('titleResults')
+  const title = document.createElement('h2')
+  gameDiv.appendChild(title)
+  title.innerHTML = game.external
+  releventResults.appendChild(gameDiv)
+}
+
+const removeLastsearch = () => {
+  const oldResults = document.querySelectorAll('.titleResults')
+  oldResults.forEach(result => result.remove())
+}
 
 const formsubmit = async (e) => {
   e.preventDefault()
@@ -15,8 +28,17 @@ const formsubmit = async (e) => {
   console.log(searchString)
   const result = await axios.get('https://www.cheapshark.com/api/1.0/games?title=' + searchString)
   console.log(result.data)
+  removeLastsearch()
+  const sortedTitles = result.data.sort((a, b) => a.external > b.external ? 1 : -1 ) 
+  sortedTitles.forEach(game => {
+    showGame(game)
+    console.log(game.external)
+  });
 }
 
 const form = document.querySelector('.searchResults')
 form.addEventListener('submit', formsubmit)
 
+const releventResults=document.querySelector('.releventResults')
+
+document.getElementById(".searchResults").value;
