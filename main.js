c7f11b8392fd21271d3f690c5202568674270ec7
Marco Arellano <marcoarellano@Marcos-MacBook-Pro-3.local>
2020-11-14 16:43:47 -0500

    Added dropdown with no functionality atm

--- main.js
+++ main.js
@@ -8,18 +8,27 @@
 // })
 //   .then(data => console.log(data))
 //   .catch(error => console.log('oops! data cannot be found.'))
+
+//<option value=".titleResults">${titleResults}</option>
+
 const showGame= (game) => {
-  const gameDiv = document.createElement('div')
-  gameDiv.classList.add('titleResults')
-  const title = document.createElement('h2')
-  gameDiv.appendChild(title)
-  title.innerHTML = game.external
-  releventResults.appendChild(gameDiv)
+  const option = document.createElement('option')
+  option.setAttribute('value', game.cheapestDealID)
+  option.innerHTML=`<img src="${game.thumb}">${game.external}`
+  // const gameDiv = document.createElement('div')
+  // gameDiv.classList.add('titleResults')
+  // const title = document.createElement('h2')
+  // gameDiv.appendChild(title)
+  // title.innerHTML = game.external
+  dropDown.appendChild(option)
 }
 
 const removeLastsearch = () => {
-  const oldResults = document.querySelectorAll('.titleResults')
-  oldResults.forEach(result => result.remove())
+  const titleResults = document.querySelector('#dropDown')
+  titleResults.childNodes.forEach(result => {
+    console.log(result)
+    titleResults.removeChild(result)
+  }) 
 }
 
 const formsubmit = async (e) => {
@@ -32,13 +41,24 @@ const formsubmit = async (e) => {
   const sortedTitles = result.data.sort((a, b) => a.external > b.external ? 1 : -1 ) 
   sortedTitles.forEach(game => {
     showGame(game)
-    console.log(game.external)
+    // console.log(game.external)
   });
 }
 
-const form = document.querySelector('.titleResults')
+
+
+const form = document.querySelector('.searchResults')
 form.addEventListener('submit', formsubmit)
+   
+const getCheapestdeal= async (e) => {
+  console.log(e.target.value)
+  const result = await axios.get('https://www.cheapshark.com/api/1.0/deals?id=' + e.target.value)
+  console.log(result)
+}
 
-const releventResults=document.querySelector('.releventResults')
 
-document.getElementById(".titleResults").value;
+
+const dropDown=document.querySelector('#dropDown')
+dropDown.addEventListener('change', getCheapestdeal)
+// document.getElementById(".searchResults").value;
+// Movie Database homework for clues on how to remove search results
