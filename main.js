2a217d90df6334898cee8252affffb3b99a0c9a1
Marco Arellano <marcoarellano@Marcos-MacBook-Pro-3.local>
2020-11-16 16:35:51 -0500

    Added Logo, centered all text, changed font.

--- main.js
+++ main.js
@@ -23,12 +23,19 @@ const showGame= (game) => {
   dropDown.appendChild(option)
 }
 
+// const removeLastsearch = () => {
+//   const titleResults = document.querySelector('#dropDown')
+//   titleResults.childNodes.forEach(result => {
+//     console.log(result)
+//     titleResults.removeChild(result)
+//   }) 
+// }
+
 const removeLastsearch = () => {
   const titleResults = document.querySelector('#dropDown')
-  titleResults.childNodes.forEach(result => {
-    console.log(result)
-    titleResults.removeChild(result)
-  }) 
+  while (titleResults.childNodes.length) {
+    titleResults.firstChild.remove()
+  }
 }
 
 const formsubmit = async (e) => {
@@ -52,8 +59,33 @@ form.addEventListener('submit', formsubmit)
    
 const getCheapestdeal= async (e) => {
   console.log(e.target.value)
+  // clear out the top deal div
+  const topDeal = document.querySelector('.topDeal')
+  topDeal.innerHTML = null
+  // go get the new game data
   const result = await axios.get('https://www.cheapshark.com/api/1.0/deals?id=' + e.target.value)
-  console.log(result)
+  console.log(result.data)
+  const gameInfo = result.data.gameInfo
+  console.log("gameInfo",gameInfo)
+  // this creates the image 
+  const gameImage = document.createElement('img')
+  gameImage.setAttribute('src', gameInfo.thumb)
+  // this creates the game title
+  const gameTitle = document.createElement('h2')
+  gameTitle.innerText = gameInfo.name
+  // this creates the deal price
+  const gamePrice = document.createElement('h3')
+  gamePrice.innerHTML="Best Available Sale Price :  <br>" + gameInfo.salePrice
+   // this creates the retail price
+   const gameRetail = document.createElement('h3')
+   gameRetail.innerHTML ="Regular Asking Price :  <br>" + gameInfo.retailPrice
+
+  // And now we plug them in 
+  topDeal.append(gameImage)
+  topDeal.append(gameTitle)
+  topDeal.append(gamePrice)
+  topDeal.append(gameRetail)
+  
 }
 
 
@@ -62,3 +94,4 @@ const dropDown=document.querySelector('#dropDown')
 dropDown.addEventListener('change', getCheapestdeal)
 // document.getElementById(".searchResults").value;
 // Movie Database homework for clues on how to remove search results
+
