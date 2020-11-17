f874f23b0bd4478865a33ea1a706b9401f5f575d
Marco Arellano <marcoarellano@Marcos-MacBook-Pro-3.local>
2020-11-17 10:58:21 -0500

    CSS update 2

--- main.js
+++ main.js
@@ -1,49 +1,12 @@
-// codepen.io template
-// JS for content editable trick from Chris Coyier
-// var span = document.querySelector("span");
 
-// span.addEventListener("input", function () {
-//   var text = this.innerText;
-//   this.setAttribute("data-heading", text);
-//   this.parentElement.setAttribute("data-heading", text);
-// });
-
-
-// fetch('https://www.cheapshark.com/api/1.0/games?id=612',
-//   method: 'POST',
-//   body: {'
-//   }
-
-// }).then(res => {
-//   return res.json()
-// })
-//   .then(data => console.log(data))
-//   .catch(error => console.log('oops! data cannot be found.'))
-
-//<option value=".titleResults">${titleResults}</option>
 const dropDown = document.querySelector('#dropDown')
 const showGame = (game) => {
   const option = document.createElement('option')
   option.setAttribute('value', game.cheapestDealID)
-  // option.innerHTML = `<img src="${game.thumb}">${game.external}`
   option.innerHTML = game.external
-  // const gameDiv = document.createElement('div')
-  // gameDiv.classList.add('titleResults')
-  // const title = document.createElement('h2')
-  // gameDiv.appendChild(title)
-  // title.innerHTML = game.external
   dropDown.appendChild(option)
 
 }
-
-// const removeLastsearch = () => {
-//   const titleResults = document.querySelector('#dropDown')
-//   titleResults.childNodes.forEach(result => {
-//     console.log(result)
-//     titleResults.removeChild(result)
-//   }) 
-// }
-
 
 // Clears last search results 
 const removeLastsearch = () => {
@@ -53,7 +16,6 @@ const removeLastsearch = () => {
   }
 }
 
-
 const formsubmit = async (e) => {
   e.preventDefault()
   const searchString = document.querySelector('#SearchInput').value
@@ -62,12 +24,10 @@ const formsubmit = async (e) => {
   console.log(result.data)
   removeLastsearch()
   const sortedTitles = result.data.sort((a, b) => a.external > b.external ? 1 : -1)
-
   const option = document.createElement('option')
   option.setAttribute("value", "default")
   option.innerHTML = "Results"
   dropDown.appendChild(option)
-
   sortedTitles.forEach(game => {
     showGame(game)
     // console.log(game.external)
